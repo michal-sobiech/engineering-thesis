58c7655b635c18fdb058225f527f4c61cfab375e
Michał Sobiech <michal.sobiech15@gmail.com>
2024-12-18 10:33:29 +0100

    chore: init spring boot and react

---
+++ backend/src/test/java/pl/michal/sobiech/engineering_thesis/EngineeringThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michal.sobiech.engineering_thesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineeringThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
