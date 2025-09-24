aeb344df0d949ecb6ebe819d0619b07305527eeb
Michał Sobiech <michal.sobiech15@gmail.com>
2025-09-24 20:33:10 +0200

    feat(frontend): create routeTemplates

---
+++ frontend/src/router/route-templates.ts
@@ -0,0 +1,9 @@
+export const routeTemplates = {
+    MAIN_PAGE: "/",
+    SIGN_UP: "/sign-up",
+    LOG_IN: "/log-in",
+    ENTREPRENEUR_LANDING_PAGE: "/entrepreneur-landing-page",
+    CREATE_ENTERPRISE: "/create-enterprise",
+    ENTERPRISE: "/enterprises/:enterpriseId",
+    CREATE_ENTERPRISE_EMPLOYEE: "/enterprises/:enterpriseId/create-employee"
+};
--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,8 +1,12 @@
+import { generatePath } from "react-router";
+import { routeTemplates } from "./route-templates";
+
 export const routes = {
-    MAIN_PAGE: "/",
-    SIGN_UP: "/sign-up",
-    LOG_IN: "/log-in",
-    ENTREPRENEUR_LANDING_PAGE: "/entrepreneur-landing-page",
-    CREATE_ENTERPRISE: "/create-enterprise",
-    ENTERPRISE: "/enterprises/:enterpriseId",
-};
+    MAIN_PAGE: routeTemplates.MAIN_PAGE,
+    SIGN_UP: routeTemplates.SIGN_UP,
+    LOG_IN: routeTemplates.LOG_IN,
+    ENTREPRENEUR_LANDING_PAGE: routeTemplates.ENTREPRENEUR_LANDING_PAGE,
+    CREATE_ENTERPRISE: routeTemplates.CREATE_ENTERPRISE,
+    ENTERPRISE: (enterpriseId: number) => generatePath(routeTemplates.ENTERPRISE, { enterpriseId }),
+    CREATE_ENTERPRISE_EMPLOYEE: (enterpriseId: number) => generatePath(routeTemplates.CREATE_ENTERPRISE_EMPLOYEE, { enterpriseId }),
+} satisfies Record<keyof typeof routeTemplates, string | ((...args: any[]) => string)>;
