010535dbf8953689ce83bb6adff3f92bb81e1fb6
Michał Sobiech <michal.sobiech15@gmail.com>
2025-09-29 11:23:38 +0200

    feat(backend): update roles

--- backend/src/main/java/pl/michal_sobiech/engineering_thesis/user/Role.java
+++ backend/src/main/java/pl/michal_sobiech/engineering_thesis/user/Role.java
@@ -1,5 +1,5 @@
 package pl.michal_sobiech.engineering_thesis.user;
 
 public enum Role {
-    ENTREPRENEUR, EMPLOYEE, CUSTOMER, ADMIN
+    ENTREPRENEUR, EMPLOYEE, CUSTOMER, REGULAR_ADMIN, HEAD_ADMIN
 }
