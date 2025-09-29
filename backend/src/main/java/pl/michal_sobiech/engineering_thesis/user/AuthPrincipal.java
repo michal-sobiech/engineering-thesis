236ff1c330a0b63819be26009b524aca5cd8a723
Michał Sobiech <michal.sobiech15@gmail.com>
2025-09-29 11:40:42 +0200

    feat(backend): revert to one role in AuthPrincipal

--- backend/src/main/java/pl/michal_sobiech/engineering_thesis/user/AuthPrincipal.java
+++ backend/src/main/java/pl/michal_sobiech/engineering_thesis/user/AuthPrincipal.java
@@ -1,6 +1,4 @@
 package pl.michal_sobiech.engineering_thesis.user;
 
-import java.util.List;
-
-public record AuthPrincipal(long userId, List<Role> roles) {
+public record AuthPrincipal(long userId, Role role) {
 }
