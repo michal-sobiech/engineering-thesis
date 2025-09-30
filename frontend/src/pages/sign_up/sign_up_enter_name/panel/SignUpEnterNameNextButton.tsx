e2ffa8ce6deb0efff92db40950a56280eda17a8d
Michał Sobiech <michal.sobiech15@gmail.com>
2025-09-30 21:58:49 +0200

    feat(frontend): add a first and last name check on sign in form

--- frontend/src/pages/sign_up/sign_up_enter_name/panel/SignUpEnterNameNextButton.tsx
+++ frontend/src/pages/sign_up/sign_up_enter_name/panel/SignUpEnterNameNextButton.tsx
@@ -1,12 +1,23 @@
 import { toast } from "react-toastify";
 import { StandardButton } from "../../../../common/StandardButton";
+import { toastError } from "../../../../utils/toast";
 import { useContextOrThrow } from "../../../../utils/useContextOrThrow";
 import { signUpWizardContext } from "../../wizard/SignUpWizardContext";
 
 export const SignUpEnterNameNextButton = () => {
-    const { incrementStep } = useContextOrThrow(signUpWizardContext);
+    const { incrementStep, firstName, lastName } = useContextOrThrow(signUpWizardContext);
 
     const onClick = async () => {
+        if (firstName === "") {
+            toastError("Please enter your first name")
+            return
+        }
+
+        if (lastName === "") {
+            toastError("Please enter your last name");
+            return
+        }
+
         toast.dismiss();
         incrementStep();
     }
