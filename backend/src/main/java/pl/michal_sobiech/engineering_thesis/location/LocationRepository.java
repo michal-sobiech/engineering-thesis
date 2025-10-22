97fd7191a24834a37803a218d881fe72c6ad19be
Michał Sobiech <michal.sobiech15@gmail.com>
2025-10-22 12:54:39 +0200

    chore(backend): remove old entity files

--- backend/src/main/java/pl/michal_sobiech/engineering_thesis/location/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.michal_sobiech.engineering_thesis.location;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-
-}
--- backend/src/main/java/pl/michal_sobiech/engineering_thesis/photo/PhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.michal_sobiech.engineering_thesis.photo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhotoRepository extends JpaRepository<Photo, Long> {
-
-}
