80250b0b364fdcb2e9f7a21e4e33af19a143afda
Michał Sobiech <michal.sobiech15@gmail.com>
2025-10-26 12:28:45 +0100

    feat(backend): jpql query

--- backend/src/main/java/pl/michal_sobiech/engineering_thesis/enterprise_service_slot/EnterpriseServiceSlotRepository.java
+++ backend/src/main/java/pl/michal_sobiech/engineering_thesis/enterprise_service_slot/EnterpriseServiceSlotRepository.java
@@ -3,9 +3,24 @@ package pl.michal_sobiech.engineering_thesis.enterprise_service_slot;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EnterpriseServiceSlotRepository extends JpaRepository<EnterpriseServiceSlotEntity, Long> {
 
     public List<EnterpriseServiceSlotEntity> findAllByEnterpriseServiceId(long enterpriseServiceId);
+
+    @Query("""
+        SELECT slot FROM EnterpriseServiceSlotEntity slot
+        JOIN EnterpriseServiceEntity service ON service.enterpriseServiceId = slot.enterpriseServiceId
+        JOIN EnterpriseEntity enterprise ON enterprise.enterpriseId = service.enterpriseId
+        WHERE (:serviceName IS NULL OR LOWER(service.name) LIKE LOWER(CONCAT('%', :serviceName, '%')))
+        AND (:enterpriseName IS NULL OR LOWER(enterprise.name) LIKE LOWER(CONCAT('%', :enterpriseName, '%')))
+        AND (:startDate IS NULL OR slot.startTime >= :startDate)
+        AND (:endDate IS NULL OR slot.endTime <= :endDate)
+        AND (:cathegory IS NULL OR service.cathegory = :cathegory)
+    """, nativeQuery = true)
+    public List<EnterpriseServiceSlotEntity> filterSlots(
+
+    )
 
 }
