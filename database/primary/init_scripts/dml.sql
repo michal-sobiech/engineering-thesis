ba643c69e5d6a2206cdcf246c0e7d5524c616091
Michał Sobiech <michal.sobiech15@gmail.com>
2025-11-09 18:27:25 +0100

    feat(database): change primary key type

--- database/primary/init_scripts/dml.sql
+++ database/primary/init_scripts/dml.sql
@@ -1,6 +1,5 @@
-INSERT INTO public.app_user (user_id, user_group, username, first_name, last_name, password_hash)
+INSERT INTO public.app_user (user_group, username, first_name, last_name, password_hash)
 VALUES (
-    1,
     'ENTREPRENEUR',
     'adam.adamczyk@example.com',
     'Adam',
@@ -8,9 +7,8 @@ VALUES (
     '$2a$14$H3Xb1uyNVBoVq7aXZI.mQO/L.iavvGIGjqsrOhm/5HRT9D0A0xkOO'
 );
 
-INSERT INTO public.app_user (user_id, user_group, username, first_name, last_name, password_hash)
+INSERT INTO public.app_user (user_group, username, first_name, last_name, password_hash)
 VALUES (
-    2,
     'CUSTOMER',
     'jan.kowalski@example.com',
     'Jan',
@@ -18,9 +16,8 @@ VALUES (
     '$2a$14$H3Xb1uyNVBoVq7aXZI.mQO/L.iavvGIGjqsrOhm/5HRT9D0A0xkOO'
 );
 
-INSERT INTO public.enterprise (enterprise_id, owner_user_id, name, description, address, latitude, longitude, suspended_by_admin)
+INSERT INTO public.enterprise (owner_user_id, name, description, address, latitude, longitude, suspended_by_admin)
 VALUES (
-    1,
     1,
     'The Good Hairdresser',
     'Description here',
@@ -30,9 +27,21 @@ VALUES (
     FALSE
 );
 
-INSERT INTO public.enterprise_service
-VALUES (
-    1,
+INSERT INTO public.enterprise_service (
+    enterprise_id,
+    name,
+    description,
+    address,
+    latitude,
+    longitude,
+    time_zone,
+    takes_custom_appointments,
+    max_distance_km,
+    cathegory,
+    price,
+    currency, 
+    suspended_by_admin
+) VALUES (
     1,
     'Female haircut',
     'A basic female haircut',
@@ -48,9 +57,21 @@ VALUES (
     FALSE
 );
 
-INSERT INTO public.enterprise_service
-VALUES (
-    2,
+INSERT INTO public.enterprise_service (
+    enterprise_id,
+    name,
+    description,
+    address,
+    latitude,
+    longitude,
+    time_zone,
+    takes_custom_appointments,
+    max_distance_km,
+    cathegory,
+    price,
+    currency, 
+    suspended_by_admin
+) VALUES (
     1,
     'Haircut at home',
     'Special offer: haircut at home',
@@ -66,23 +87,22 @@ VALUES (
     FALSE
 );
 
-INSERT INTO public.enterprise_service_slot_template
-VALUES(1, 1, 1, '12:00:00', '12:30:00', 1);
+INSERT INTO public.enterprise_service_slot_template (enterprise_service_id, day_of_week, start_time, end_time, max_occupancy)
+VALUES(1, 1, '12:00:00', '12:30:00', 1);
 
-INSERT INTO public.enterprise_service_slot_template
-VALUES(2, 1, 1, '12:30:00', '13:00:00', 1);
+INSERT INTO public.enterprise_service_slot_template (enterprise_service_id, day_of_week, start_time, end_time, max_occupancy)
+VALUES(1, 1, '12:30:00', '13:00:00', 1);
 
-INSERT INTO public.enterprise_service_slot_template
-VALUES(3, 1, 3, '13:00:00', '13:30:00', 1);
+INSERT INTO public.enterprise_service_slot_template (enterprise_service_id, day_of_week, start_time, end_time, max_occupancy)
+VALUES(1, 3, '13:00:00', '13:30:00', 1);
 
-INSERT INTO public.enterprise_service_slot_template
-VALUES(4, 1, 3, '13:30:00', '14:00:00', 1);
+INSERT INTO public.enterprise_service_slot_template (enterprise_service_id, day_of_week, start_time, end_time, max_occupancy)
+VALUES(1, 3, '13:30:00', '14:00:00', 1);
 
-INSERT INTO public.enterprise_service_slot_template
-VALUES(5, 2, 3, '8:00:00', '18:00:00', NULL);
+INSERT INTO public.enterprise_service_slot_template (enterprise_service_id, day_of_week, start_time, end_time)
+VALUES(2, 3, '8:00:00', '18:00:00');
 
 INSERT INTO public.appointment (
-    appointment_id,
     enterprise_service_id,
     customer_user_id,
     price,
@@ -90,7 +110,6 @@ INSERT INTO public.appointment (
     end_time,
     is_custom
 ) VALUES (
-    1,
     1,
     2,
     100,
@@ -100,7 +119,6 @@ INSERT INTO public.appointment (
 );
 
 INSERT INTO public.appointment (
-    appointment_id,
     enterprise_service_id,
     customer_user_id,
     price,
@@ -114,7 +132,6 @@ INSERT INTO public.appointment (
 ) VALUES (
     2,
     2,
-    2,
     300,
     '2025-12-03 13:00:00+00',
     '2025-12-03 14:00:00+00',
@@ -125,9 +142,13 @@ INSERT INTO public.appointment (
     21.011346
 );
 
-INSERT INTO public.review
-VALUES (
-    1,
+INSERT INTO public.review (
+    creator_customer_user_id,
+    subject_enterprise_service_id,
+    stars_out_of_5,
+    content,
+    suspended_by_admin
+) VALUES (
     2,
     1,
     4,
@@ -135,12 +156,13 @@ VALUES (
     FALSE
 );
 
-INSERT INTO public.report
+INSERT INTO public.report (
+    creator_user_id,
+    enterprise_id,
+    is_resolved
+)
 VALUES (
-    1,
     2,
     1,
-    NULL,
-    NULL,
     FALSE
 );
