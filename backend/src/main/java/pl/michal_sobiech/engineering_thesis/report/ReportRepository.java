74e5c52a0f0aa92e5847fcc30d6009f3f8bf9d26
Michał Sobiech <michal.sobiech15@gmail.com>
2025-11-12 19:52:26 +0100

    fix(backend): jpql grammar errors

--- backend/src/main/java/pl/michal_sobiech/engineering_thesis/report/ReportRepository.java
+++ backend/src/main/java/pl/michal_sobiech/engineering_thesis/report/ReportRepository.java
@@ -9,46 +9,46 @@ import org.springframework.data.repository.query.Param;
 
 public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
 
-    @Query("""
-            SELECT
-            r.reportId AS reportId,
-            u.userGroup AS creatorUserGroup,
-            r.creatorUserId AS creatorUserId,
-            u.username AS creatorUsername
-            r.enterpriseId AS enterpriseId,
-            r.enterpriseServiceId AS enterpriseServiceId,
-            r.reviewId AS reviewId,
-            r.isResolved AS isResolved
-            FROM ReportEntity r
-            JOIN UserEntity u on u.userId = r.creatorUserId
-            WHERE r.isResolved = FALSE
-            """)
-    public List<GetUnresolvedReportsRow> findDetailedUnresolvedReports();
+        @Query("""
+                        SELECT
+                        r.reportId AS reportId,
+                        u.userGroup AS creatorUserGroup,
+                        r.creatorUserId AS creatorUserId,
+                        u.username AS creatorUsername,
+                        r.enterpriseId AS enterpriseId,
+                        r.enterpriseServiceId AS enterpriseServiceId,
+                        r.reviewId AS reviewId,
+                        r.isResolved AS isResolved
+                        FROM ReportEntity r
+                        JOIN UserEntity u on u.userId = r.creatorUserId
+                        WHERE r.isResolved = FALSE
+                        """)
+        public List<GetUnresolvedReportsRow> findDetailedUnresolvedReports();
 
-    @Query("""
-            SELECT
-            r.reportId AS reportId,
-            u.userGroup AS creatorUserGroup,
-            r.creatorUserId AS creatorUserId,
-            u.username AS creatorUsername
-            r.enterpriseId AS enterpriseId,
-            r.enterpriseServiceId AS enterpriseServiceId,
-            r.reviewId AS reviewId,
-            r.isResolved AS isResolved
-            FROM ReportEntity r
-            JOIN UserEntity u on u.userId = r.creatorUserId
-            WHERE r.isResolved = FALSE
-            AND r.reportId = :reportId
-            """)
-    public Optional<GetUnresolvedReportsRow> findDetailedUnresolvedReportByReportId(
-            @Param("reportId") long reportId);
+        @Query("""
+                        SELECT
+                        r.reportId AS reportId,
+                        u.userGroup AS creatorUserGroup,
+                        r.creatorUserId AS creatorUserId,
+                        u.username AS creatorUsername,
+                        r.enterpriseId AS enterpriseId,
+                        r.enterpriseServiceId AS enterpriseServiceId,
+                        r.reviewId AS reviewId,
+                        r.isResolved AS isResolved
+                        FROM ReportEntity r
+                        JOIN UserEntity u on u.userId = r.creatorUserId
+                        WHERE r.isResolved = FALSE
+                        AND r.reportId = :reportId
+                        """)
+        public Optional<GetUnresolvedReportsRow> findDetailedUnresolvedReportByReportId(
+                        @Param("reportId") long reportId);
 
-    @Query("""
-            UPDATE ReportEntity r
-            SET r.isResolved = TRUE
-            WHERE r.reportId = :reportId
-            AND r.isResolved = FALSE
-            """)
-    public int resolveReport(@Param("reportId") long reportId);
+        @Query("""
+                        UPDATE ReportEntity r
+                        SET r.isResolved = TRUE
+                        WHERE r.reportId = :reportId
+                        AND r.isResolved = FALSE
+                        """)
+        public int resolveReport(@Param("reportId") long reportId);
 
 }
