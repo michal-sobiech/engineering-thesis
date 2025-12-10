ec30f4e99d0538c6b549bdcc7562ad8b1ae8319a
Michał Sobiech <michal.sobiech15@gmail.com>
2025-12-10 11:42:01 +0100

    feat(frontend): update WeeklyCalendar

--- frontend/src/common/calendar/WeeklyCalendar.tsx
+++ frontend/src/common/calendar/WeeklyCalendar.tsx
@@ -1,48 +1,29 @@
 import { FC } from "react";
-import { Calendar, SlotInfo } from "react-big-calendar";
+import { Calendar, CalendarProps } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { UseStateSetter } from "../../utils/use-state";
-import { localizer } from "../localizer";
-import { EventWithId } from "./EventWithId";
 
-export interface WeeklyCalendarProps {
-    events: EventWithId[];
-    setEvents: UseStateSetter<EventWithId[]>;
-    onSelectSlot?: (slot: SlotInfo) => void;
-}
+export const WeeklyCalendar: FC<CalendarProps> = (props) => {
+    const { localizer, ...nonRequiredProps } = props;
 
-export const WeeklyCalendar: FC<WeeklyCalendarProps> = ({ events, setEvents, onSelectSlot }) => {
-    const defaultOnSelectSlot = (slot: SlotInfo) => {
-        const event: EventWithId = {
-            start: slot.start,
-            end: slot.end,
-            resource: { tempId: crypto.randomUUID() }
-        }
-        setEvents([...events, event]);
-    }
-    onSelectSlot = onSelectSlot ?? defaultOnSelectSlot;
+    const finalRequiredProps: CalendarProps = {
+        localizer: props.localizer ?? localizer
+    };
 
-    function deleteEvent(eventToDelete: EventWithId) {
-        setEvents(previousEvents => previousEvents.filter(event => {
-            return event.resource.tempId != eventToDelete.resource.tempId;
-        }));
-    }
-
-    const onSelectEvent = (event: EventWithId) => {
-        deleteEvent(event);
-    }
-
-    return <Calendar
-        localizer={localizer}
-        events={events}
-        views={["week"]}
-        defaultView="week"
-        selectable
-        toolbar={false}
-        onSelectSlot={onSelectSlot}
-        components={{
+    const finalOptionalProps: CalendarProps = {
+        views: ["week"],
+        defaultView: "week",
+        selectable: true,
+        toolbar: false,
+        components: {
             header: ({ label }) => <span>{label.split(" ")[1]}</span>
-        }}
-        onSelectEvent={onSelectEvent}
-    />;
+        },
+        ...props
+    };
+
+    const finalProps: CalendarProps = {
+        ...finalRequiredProps,
+        ...finalOptionalProps
+    };
+
+    return <Calendar {...finalProps} />;
 }
